beff9d70ebae0744327c93452788d42170b7abd0
Hyungsuk Hong <aanoaa@gmail.com>
2016-01-21 14:57:46 +0900

    Setting mojo startup

--- lib/OpenCloset/Donation.pm
+++ lib/OpenCloset/Donation.pm
@@ -1,18 +1,44 @@
 package OpenCloset::Donation;
 use Mojo::Base 'Mojolicious';
 
-# This method will run once at server start
+=head1 METHODS
+
+=head2 startup
+
+This method will run once at server start
+
+=cut
+
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    $self->plugin('Config');
+    $self->plugin('OpenCloset::Plugin::Helpers');
+    $self->secrets( [$ENV{OPENCLOSET_DONATION_SECRET} || time] );
+    $self->sessions->cookie_name( $self->app->moniker );
+    $self->sessions->default_expiration(86400);
 
-  # Router
-  my $r = $self->routes;
-
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    $self->_assets;
+    $self->_public_routes;
+    $self->_private_routes;
 }
+
+sub _assets {
+    my $self = shift;
+
+    $self->defaults( jses => [], csses => [] );
+}
+
+sub _public_routes {
+    my $self = shift;
+    my $r    = $self->routes;
+
+    $r->get('/')->to( cb => sub { shift->redirect_to('add') } );
+
+    $r->get('/new')->to('root#add')->name('add');
+    $r->post('/')->to('root#create')->name('create');
+}
+
+sub _private_routes { }
 
 1;
