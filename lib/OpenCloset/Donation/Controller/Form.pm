a4f641ace1f28c9c0b3bbe690cb8a503bac51741
Hyungsuk Hong <aanoaa@gmail.com>
2016-02-01 12:35:13 +0900

    Add search form

--- lib/OpenCloset/Donation/Controller/Form.pm
+++ lib/OpenCloset/Donation/Controller/Form.pm
@@ -18,12 +18,14 @@ sub list {
     my $self = shift;
     my $p    = $self->param('p') || 1;
     my $s    = $self->param('s') || '';
+    my $q    = $self->param('q');
 
     my $cond = $s eq '' ? {} : { status => $s };
+    my $attr = { page => $p, rows => 20, order_by => { -desc => 'update_date' } };
     $s = undef if $s eq 'null';
 
-    my $rs = $self->schema->resultset('DonationForm')
-        ->search( $cond, { page => $p, rows => 20, order_by => { -desc => 'update_date' } } );
+    $cond = $self->_search_cond($q) if $q;
+    my $rs = $self->schema->resultset('DonationForm')->search( $cond, $attr );
 
     my $pager   = $rs->pager;
     my $pageset = Data::Pageset->new(
@@ -83,6 +85,36 @@ sub update_form {
 
     $form->update( { parcel_service => $parcel_service, waybill => $waybill, status => 'sent' } );
     $self->redirect_to('form');
+}
+
+=head2 _search_cond($q)
+
+=cut
+
+sub _search_cond {
+    my ( $self, $q ) = @_;
+
+    return {} unless length $q > 1;
+
+    my @or;
+    if ( $q =~ /^[0-9\-]+$/ ) {
+        $q =~ s/-//g;
+        push @or, { 'phone' => { like => "%$q%" } };
+    }
+    elsif ( $q =~ /^[a-zA-Z0-9_\-]+/ ) {
+        if ( $q =~ /\@/ ) {
+            push @or, { email => { like => "%$q%" } };
+        }
+        else {
+            push @or, { email => { like => "%$q%" } };
+            push @or, { name  => { like => "%$q%" } };
+        }
+    }
+    elsif ( $q =~ m/^[ㄱ-힣]+$/ ) {
+        push @or, { name => { like => "$q%" } };
+    }
+
+    return { -or => [@or] };
 }
 
 1;
