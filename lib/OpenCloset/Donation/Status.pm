375dda320749db427ba5d86847e1934c113ec965
Hyungsuk Hong <aanoaa@gmail.com>
2016-02-11 18:17:45 +0900

    Add status `delivered`

--- lib/OpenCloset/Donation/Status.pm
+++ lib/OpenCloset/Donation/Status.pm
@@ -5,6 +5,7 @@ use utf8;
 our $NULL          = '';
 our $WAITING       = 'waiting';
 our $DELIVERING    = 'delivering';
+our $DELIVERED     = 'delivered';
 our $RETURNED      = 'returned';
 our $CANCEL        = 'cancel';
 our $DO_NOT_RETURN = 'do-not-return';
@@ -13,21 +14,23 @@ our $REGISTERED    = 'registered';
 our $LABEL_NULL          = '신청됨';
 our $LABEL_WAITING       = '발송대기';
 our $LABEL_DELIVERING    = '배송중';
+our $LABEL_DELIVERED     = '배송완료';
 our $LABEL_RETURNED      = '반송완료';
 our $LABEL_CANCEL        = '취소';
 our $LABEL_DO_NOT_RETURN = '반송안함';
 our $LABEL_REGISTERED    = '등록됨';
 
-our @ALL = ( $NULL, $WAITING, $DELIVERING, $RETURNED, $CANCEL, $DO_NOT_RETURN, $REGISTERED );
+our @ALL = ( $NULL, $WAITING, $DELIVERING, $DELIVERED, $RETURNED, $CANCEL, $DO_NOT_RETURN, $REGISTERED );
 our @ALL_LABEL = (
-    $LABEL_NULL, $LABEL_WAITING, $LABEL_DELIVERING, $LABEL_RETURNED, $LABEL_CANCEL, $LABEL_DO_NOT_RETURN,
-    $LABEL_REGISTERED
+    $LABEL_NULL,     $LABEL_WAITING, $LABEL_DELIVERING,    $LABEL_DELIVERED,
+    $LABEL_RETURNED, $LABEL_CANCEL,  $LABEL_DO_NOT_RETURN, $LABEL_REGISTERED
 );
 
 our %LABEL_MAP = (
     $NULL          => $LABEL_NULL,
     $WAITING       => $LABEL_WAITING,
     $DELIVERING    => $LABEL_DELIVERING,
+    $DELIVERED     => $LABEL_DELIVERED,
     $RETURNED      => $LABEL_RETURNED,
     $CANCEL        => $LABEL_CANCEL,
     $DO_NOT_RETURN => $LABEL_DO_NOT_RETURN,
