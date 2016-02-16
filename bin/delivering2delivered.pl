d0a6cb4231dac4766db600cfc1a44581a56f3d6c
Hyungsuk Hong <aanoaa@gmail.com>
2016-02-16 16:33:39 +0900

    perltidy bin/delivering2delivered.pl

--- bin/delivering2delivered.pl
+++ bin/delivering2delivered.pl
@@ -12,8 +12,7 @@ binmode STDOUT, ':utf8';
 
 my $config = require 'donation.conf';
 my $conf   = $config->{database};
-my $schema = OpenCloset::Schema->connect(
-    { dsn => $conf->{dsn}, user => $conf->{user}, password => $conf->{pass}, %{ $conf->{opts} } } );
+my $schema = OpenCloset::Schema->connect( { dsn => $conf->{dsn}, user => $conf->{user}, password => $conf->{pass}, %{ $conf->{opts} } } );
 
 my $driver = 'KR::CJKorea';
 my $rs = $schema->resultset('DonationForm')->search( { status => $OpenCloset::Donation::Status::DELIVERING } );
@@ -29,6 +28,5 @@ while ( my $row = $rs->next ) {
     next unless $latest =~ /배달완료/;
 
     $row->update( { status => $OpenCloset::Donation::Status::DELIVERED } );
-    printf "[%d] %s: %s -> %s\n", $row->id, $row->name, $OpenCloset::Donation::Status::DELIVERING,
-        $OpenCloset::Donation::Status::DELIVERED;
+    printf "[%d] %s: %s -> %s\n", $row->id, $row->name, $OpenCloset::Donation::Status::DELIVERING, $OpenCloset::Donation::Status::DELIVERED;
 }
