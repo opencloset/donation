723d9173a330dc6d9681a4c3988cf7fdcd3c69b6
Hyungsuk Hong <aanoaa@gmail.com>
2016-04-21 14:46:40 +0900

    Add new category `misc`

--- lib/OpenCloset/Donation/Category.pm
+++ lib/OpenCloset/Donation/Category.pm
@@ -13,6 +13,7 @@ our $SHOES     = 'shoes';
 our $SKIRT     = 'skirt';
 our $TIE       = 'tie';
 our $WAISTCOAT = 'waistcoat';
+our $MISC      = 'misc';
 
 our $LABEL_BELT      = '벨트';
 our $LABEL_BLOUSE    = '블라우스';
@@ -25,12 +26,13 @@ our $LABEL_SHOES     = '구두';
 our $LABEL_SKIRT     = '스커트';
 our $LABEL_TIE       = '타이';
 our $LABEL_WAISTCOAT = '조끼';
+our $LABEL_MISC      = '기타';
 
-our @ALL = ( $BELT, $BLOUSE, $COAT, $JACKET, $ONEPIECE, $PANTS, $SHIRT, $SHOES, $SKIRT, $TIE, $WAISTCOAT );
+our @ALL = ( $BELT, $BLOUSE, $COAT, $JACKET, $ONEPIECE, $PANTS, $SHIRT, $SHOES, $SKIRT, $TIE, $WAISTCOAT, $MISC );
 
 our @ALL_LABEL = (
-    $LABEL_BELT, $LABEL_BLOUSE, $LABEL_COAT, $LABEL_JACKET, $LABEL_ONEPIECE, $LABEL_PANTS, $LABEL_SHIRT, $LABEL_SHOES, $LABEL_SKIRT,
-    $LABEL_TIE,  $LABEL_WAISTCOAT
+    $LABEL_BELT, $LABEL_BLOUSE,    $LABEL_COAT, $LABEL_JACKET, $LABEL_ONEPIECE, $LABEL_PANTS, $LABEL_SHIRT, $LABEL_SHOES, $LABEL_SKIRT,
+    $LABEL_TIE,  $LABEL_WAISTCOAT, $LABEL_MISC
 );
 
 our %LABEL_MAP = (
@@ -44,7 +46,8 @@ our %LABEL_MAP = (
     $SHOES     => $LABEL_SHOES,
     $SKIRT     => $LABEL_SKIRT,
     $TIE       => $LABEL_TIE,
-    $WAISTCOAT => $LABEL_WAISTCOAT
+    $WAISTCOAT => $LABEL_WAISTCOAT,
+    $MISC      => $LABEL_MISC,
 );
 
 our %PRICE = (
@@ -58,7 +61,8 @@ our %PRICE = (
     $SHOES     => 5000,
     $SKIRT     => 10000,
     $TIE       => 0,
-    $WAISTCOAT => 5000
+    $WAISTCOAT => 5000,
+    $MISC      => 0,
 );
 
 1;
