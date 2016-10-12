d2398ef8c68ece1257afb3150336bb0e79c74693
Hyungsuk Hong <aanoaa@gmail.com>
2016-10-12 13:11:55 +0900

    Auth staff only

--- lib/OpenCloset/Donation/Controller/User.pm
+++ lib/OpenCloset/Donation/Controller/User.pm
@@ -19,13 +19,23 @@ has schema => sub { shift->app->schema };
 sub auth {
     my $self = shift;
 
-    unless ( $self->session('access_token') ) {
+    my $user_id = $self->session('access_token');
+    unless ($user_id) {
         my $login = Mojo::URL->new( $self->config->{opencloset}{login} );
         $login->query( return => $self->req->url->to_abs );
         $self->redirect_to($login);
         return;
     }
 
+    my $user = $self->schema->resultset('User')->find( { id => $user_id } );
+    return unless $user;
+
+    my $user_info = $user->user_info;
+    unless ( $user_info->staff ) {
+        my $email = $user->email;
+        $self->log->warn("oops! $email is not a staff");
+        return;
+    }
     return 1;
 }
 
