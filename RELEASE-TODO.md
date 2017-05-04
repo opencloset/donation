7b26c5edba8fca43eb13b2dff91f479e63301b47
Hyungsuk Hong <aanoaa@gmail.com>
2017-05-04 16:33:18 +0900

    Bumpup version

--- RELEASE-TODO.md
+++ RELEASE-TODO.md
@@ -1,3 +1,5 @@
+v0.4.10
+
     $ mysql -u opencloset -p opencloset < OpenCloset-Schema/db/alter/128-add-status.sql
     $ closetpan OpenCloset::Common    # v0.1.1
     $ grunt
--- lib/OpenCloset/Donation.pm
+++ lib/OpenCloset/Donation.pm
@@ -7,7 +7,7 @@ use HTTP::Tiny;
 
 use OpenCloset::Schema;
 
-use version; our $VERSION = qv("v0.4.9");
+use version; our $VERSION = qv("v0.4.10");
 
 has schema => sub {
     my $self = shift;
